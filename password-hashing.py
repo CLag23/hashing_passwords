cd3f5d1cdd0149165e1bb081a4bba7d4c0709aae
Carls Laguerre <166565679+CLag23@users.noreply.github.com>
2025-09-20 11:43:16 -0400

    Update password-hashing.py
    - hashed password
    - check password when login

--- password-hashing.py
+++ password-hashing.py
@@ -1,4 +1,4 @@
-import re # regex checks patterns
+import re 
 import bcrypt
 
 def get_user_info():
@@ -18,14 +18,38 @@ def get_user_info():
             if not re.search(r"[A-Z]", password):
                 raise ValueError("Password must contain at least one uppercase letter.")
             if not re.search(r"[a-z]", password):
-                raise ValueError("Password must contain at least one lowercse letter.")
+                raise ValueError("Password must contain at least one lowercase letter.")
             if not re.search(r"[0-9]", password):
                 raise ValueError("Password must contain at least one digit.")
             if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
-                raise ValueError("Pssword must contain at least one special charcter")
+                raise ValueError("Password must contain at least one special character")
             
             return username, password
         except ValueError as e:
             print(f"Error: {e}. Please try again.")
 
 
+def hash_password(password):
+
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode("utf-8"), salt)
+    return hashed 
+
+def check_password(password, hashed):
+    return bcrypt.checkpw(password.encode("utf-8"), hashed)
+
+def login(hashed):
+    password = input("\nEnter password to login: ").strip()
+    if check_password(password, hashed):
+        print("\nLogin Sucessful!")
+    else:
+        print("\nLogin Failed!")
+
+
+if __name__ == "__main__":
+    username, password = get_user_info()
+    hashed_password = hash_password(password)
+    login_attempt = login(hashed_password)
+
+    print(f"\nUsername: {username}")
+    print(f"hashed password: {hashed_password.decode('utf-8')}")
